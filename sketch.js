2dd4089a27fb2efab329a93d7eaa656156713444
riyavaiday12 <89917314+riyavaiday12@users.noreply.github.com>
2022-01-03 20:53:53 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,30 +1,42 @@
-var canvas, backgroundImage;
-
-var gameState = 0;
-var contestantCount;
-var allContestants;
-var answer;
-var database;
-
-var question, contestant, quiz;
-
-
-function setup(){
-  canvas = createCanvas(850,400);
-  database = firebase.database();
-  quiz = new Quiz();
-  quiz.getState();
-  quiz.start();
+var canvas;
+var backgroundImage, bgImg, animal1_img, animal2_img, track;
+var database, gameState;
+var form, player, playerCount;
+var allPlayers, animal1, animal2,bush, grass;
+var animals = [];
+var bushImage, grassImage;
+//BP
+function preload() {
+  backgroundImage = loadImage("./assets/background.jpg");
+  animal1_img = loadImage("../assets/animal1.png");
+  animal2_img = loadImage("../assets/animal2.png");
+  track = loadImage("../assets/track.png");
+  bushImage = loadImage("./assets/bush.png");
+ grassImage = loadImage("./assets/grass.png");
 }
 
+//BP
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+ 
+}
 
-function draw(){
-  background("pink");
-  if(contestantCount === 2){
-    quiz.update(1);
+//BP
+function draw() {
+  background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1);
   }
-  if(gameState === 1){
-    clear();
-    quiz.play();
+
+  if (gameState === 1) {
+    game.play();
   }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
